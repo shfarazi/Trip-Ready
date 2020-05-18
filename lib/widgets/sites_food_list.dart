3f0cce90863427770934aefb4cf654fa05888c72
Shannon Farazi <shannonfarazi@gmail.com>
2020-05-17 20:56:24 -0700

    converting sitesfood to stateless widget

--- lib/widgets/sites_food_list.dart
+++ lib/widgets/sites_food_list.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:capstone/tripready.dart';
 
-class SitesFoodList extends StatefulWidget {
+class SitesFoodList extends StatelessWidget {
   final DestinationModel destination;
   final String category;
 
@@ -11,92 +11,87 @@ class SitesFoodList extends StatefulWidget {
       : super(key: key);
 
   @override
-  _SitesFoodListState createState() => _SitesFoodListState();
-}
-
-class _SitesFoodListState extends State<SitesFoodList> {
-  Future<QuerySnapshot> getFavorites() async {
-    var uid = await AuthenticationService.currentUserId();
-
-    return Firestore.instance
-        .collection('users')
-        .document(uid)
-        .collection('destinations')
-        .document(widget.destination.documentID)
-        .collection('favorites')
-        .getDocuments();
-  }
-
-  Stream<QuerySnapshot> buildQueryStream(String destinationId) {
-    return Firestore.instance
-        .collection('destinations')
-        .document(destinationId)
-        .collection('activities')
-        .orderBy('name')
-        .snapshots();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    var destinationId = this.widget.destination.documentID;
-
-    var stream = buildQueryStream(destinationId);
+    var destinationId = destination.documentID;
 
     return Container(
       child: FutureBuilder(
-        future: getFavorites(),
-        builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot> favoritesSnapshot) {
+        future: AuthenticationService.currentUserId(),
+        builder: (BuildContext context, AsyncSnapshot<String> uid) {
+          if (!uid.hasData) {
+            return CircularProgressIndicator();
+          }
+
           return StreamBuilder(
-              stream: stream,
-              builder: (context, snapshot) {
-                if (snapshot.hasData && snapshot.data.documents.length > 0) {
-                  return ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    itemCount: snapshot.data.documents.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      var snapshotItem = snapshot.data.documents[index];
-                      var activity = ActivityModel.fromSnapshot(snapshotItem);
+              stream: Firestore.instance
+                  .collection('users')
+                  .document(uid.data)
+                  .collection('destinations')
+                  .document(destination.documentID)
+                  .collection('favorites')
+                  .snapshots(),
+              builder: (context, favoritesSnapshot) {
+                if (!favoritesSnapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
 
-                      var isFavorite = false;
+                return StreamBuilder(
+                  stream: Firestore.instance
+                      .collection('destinations')
+                      .document(destinationId)
+                      .collection('activities')
+                      .orderBy('name')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData ||
+                        snapshot.data.documents.length == 0) {
+                      return Center(
+                          child: Column(children: [
+                        Text(
+                            'No favorites. Please favorite an activity to show it here.')
+                      ]));
+                    }
 
-                      if (favoritesSnapshot.hasData) {
-                        isFavorite = favoritesSnapshot.data.documents.any(
+                    return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        var snapshotItem = snapshot.data.documents[index];
+                        var activity = ActivityModel.fromSnapshot(snapshotItem);
+
+                        var isFavorite = favoritesSnapshot.data.documents.any(
                             (element) =>
                                 element.documentID == activity.documentID);
-                      }
 
-                      var showTile = widget.category == ActivityCategories.favorites && isFavorite || 
-                                     activity.category == widget.category;
+                        var showTile =
+                            category == ActivityCategories.favorites &&
+                                    isFavorite ||
+                                activity.category == category;
 
-                      return Visibility(
-                        visible: showTile,
-                        child: PhotoListViewTile(
-                            context: context,
-                            title: activity.name,
-                            subtitle: activity.type,
-                            imageUrl: activity.imageUrl,
-                            isFavorite: isFavorite,
-                            onFavorite: () async { 
-                              await FavoritesService.toggleFavorite(widget.destination.documentID, activity.documentID);
-                              setState(() {});
-                            },
-                            route: MaterialPageRoute(
-                              builder: (_) => SitesFoodDetailScreen(
-                                destination: widget.destination,
-                                activity: activity,
-                              ),
-                            )),
-                      );
-                    },
-                  );
-                } else {
-                  return Center(
-                      child: Column(children: [
-                    Text(
-                        'No favorites. Please favorite an activity to show it here.')
-                  ]));
-                }
+                        return Visibility(
+                          visible: showTile,
+                          child: PhotoListViewTile(
+                              context: context,
+                              title: activity.name,
+                              subtitle: activity.type,
+                              imageUrl: activity.imageUrl,
+                              isFavorite: isFavorite,
+                              onFavorite: () async {
+                                await FavoritesService.toggleFavorite(
+                                    destination.documentID,
+                                    activity.documentID);
+                              },
+                              route: MaterialPageRoute(
+                                builder: (_) => SitesFoodDetailScreen(
+                                  destination: destination,
+                                  activity: activity,
+                                ),
+                              )),
+                        );
+                      },
+                    );
+                  },
+                );
               });
         },
       ),
